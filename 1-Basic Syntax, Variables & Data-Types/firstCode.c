bfefaa883d064ba01b58a74fd5ab1d21db16eec3
arnabw <arnabsahawrk@gmail.com>
2024-09-28 23:52:42 +0600

    Basic Syntax, Variables & Data-Types

---
+++ 1-Basic Syntax, Variables & Data-Types/firstCode.c
@@ -0,0 +1,20 @@
+/*
+start writing code in c                  -> Documentation Section
+*/
+
+#include<stdio.h>                    //-> linking/pre processing section
+
+
+//defintion section
+
+//global declaration section
+
+//main function section
+
+//sub program section
+
+int main(){
+    printf("Hello C Programming\n");
+
+    return 0;
+}
