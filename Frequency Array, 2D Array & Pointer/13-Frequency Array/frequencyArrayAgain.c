fa437a787a13d0ba8d1652b5786ff9c49768915e
arnabw <arnabsahawrk@gmail.com>
2024-10-26 23:26:04 +0600

    10/26/24

---
+++ Frequency Array, 2D Array & Pointer/13-Frequency Array/frequencyArrayAgain.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int frequencyArray[100001] = {0};
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+        frequencyArray[array[i]]++;
+    }
+
+    for (int i = 1; i < m; i++)
+    {
+        printf("%d\n", frequencyArray[i]);
+    }
+}
